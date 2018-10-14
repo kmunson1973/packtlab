4e6cfed35dbb0f766194b0fc151bf1a271db6cde
Kent Munson <kmunson@gmail.com>
2018-10-14 10:45:21 -0700

    Update

--- k8smstrpods.sh
+++
@@ -1,3 +0,0 @@
-kubectl create -f /vagrant/packtlab/kube/serviceacct.yaml
-kubectl create clusterrolebinding packt-admin --clusterrole=cluster-admin --user="system:serviceaccount:default:packt-admin
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
